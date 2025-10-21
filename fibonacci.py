a75d96f83e09f13ca0aa7a5360142805becbea01
AffKha01 <affan01@uoguelph.ca>
2025-10-20 21:37:31 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,13 +5,21 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
-while (True):
-  print("Please enter the number of terms")
-  terms = int(input())
-  if (terms <= 0):
-    print("Incorrect input, please input a integer greater than 0")
-  else:
-    break
+
+
+def user_input():
+  while (True):
+    print("Please enter the number of terms")
+    terms = input()
+    if (terms.isdigit()):
+        terms = int(terms)
+        if (terms > 0):
+          break;
+        else:
+          print("Incorrect input, please input a integer greater than 0")
+    else:
+      print("Incorrect input, please input an integer greater than 0")
+  return terms
 
 def fibonacci (terms):
   if (terms == 0):
@@ -23,10 +31,13 @@ def fibonacci (terms):
   else:
     return fibonacci(terms - 1) + fibonacci (terms - 2)
 
-for i in range (1, terms + 1):
-  print(fibonacci(i), end= " ")
+def print_fib (terms):
+  for i in range (1, terms + 1):
+    print(fibonacci(i), end= " ")
   
-print("")
+terms = user_input()
+fibonacci(terms)
+print_fib(terms)
   
   
 # your code breaks with a string input. -1
